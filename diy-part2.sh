4cab9c6ddb4bb3765d5c368444e0627f247f2109
qazokm2580 <51231089+qazokm2580@users.noreply.github.com>
2021-01-02 13:09:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
+# 本脚本工作目录必须是git仓库的主目录
+
+# Add Some Package
+
+mkdir -p package/custom
+cd package/custom
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+git clone https://github.com/tty228/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome
+git clone -b lede https://github.com/pymumu/luci-app-smartdns
+git clone https://github.com/pymumu/openwrt-smartdns
+git clone -b 18.06  https://github.com/jerrykuku/luci-theme-argon
+git clone https://github.com/vernesong/OpenClash
+git clone https://github.com/Lienol/openwrt-package
+git clone https://github.com/fw876/helloworld
+git clone https://github.com/kenzok8/openwrt-packages
+git clone https://github.com/kenzok8/small
+
+cd ../../
+
+./scripts/feeds update -a
+./scripts/feeds install -a
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168/10.0/g' package/base-files/files/bin/config_generate
+
+# Modify some default settings
+
+#\cp -rf ../lede/zzz-default-settings package/lean/default-settings/files/zzz-default-settings
+curl -fsSL https://raw.githubusercontent.com/1orz/My-action/master/lean-lede/zzz-default-settings > package/lean/default-settings/files/zzz-default-settings
